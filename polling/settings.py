e32e03d5b747c6f8f83f14a911029a1248731cd4
ShaktiSR <44542639+ShaktiSR@users.noreply.github.com>
2022-12-20 15:18:58 +0530

    Update settings.py

--- polling/settings.py
+++ polling/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ix8$9^(j3^07$cojtn4zn4r8y$5rk^e=mo-hjt4)^mwkz2$%#m'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
